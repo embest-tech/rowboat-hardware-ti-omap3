6d3c6e6528c521462e526e3d32ec5313f97a97a3
Yegor Yelizarov <yegor_yelizarov@mentor.com>
2010-07-16 16:36:47 +0400

    Revert "Don't use hw acceleration in this branch of the project"
    This reverts commit ba89f0102875617a95b212c7469d04bda6dfacc0.

--- Android.mk
+++ Android.mk
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-#Don't use hw acceleration in this branch of the project
 ifeq ($(TARGET_BOARD_PLATFORM),omap3)
-#  include $(all-subdir-makefiles)
+  include $(all-subdir-makefiles)
 endif
